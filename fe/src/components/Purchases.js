14af2e861cb8e61b8efc0118e011fbca167825aa
joaovgamaamaral <joaovgamaamaral@gmail.com>
2019-12-08 10:22:09 -0800

    Purchase table

--- fe/src/components/Purchases.js
+++ fe/src/components/Purchases.js
@@ -1,7 +1,117 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 
-const Purchases = () => {
-    return(<h1>Purchases</h1>)
+const columns = [
+  { id: 'salesOrder', label: 'Sales\u00a0Order', minWidth: 170 },
+  { id: 'products', label: 'Products', minWidth: 100 },
+];
+
+function createData(salesOrder, products) {
+  return { salesOrder, products};
 }
 
-export default Purchases;
+const rows = [
+  createData('Sale 1', [["Product 1", 1000], ["Product 2", 2000], ["Product 3", 3000]]),
+  createData('Sale 2', [["Product 4", 4000], ["Product 5", 5000], ["Product 6", 6000]]),
+  createData('Sale 3', [["Product 7", 7000], ["Product 8", 8000], ["Product 9", 9000]]),
+  createData('Sale 4', [["Product 10", 10000], ["Product 11", 11000], ["Product 12", 12000]]),
+  createData('Sale 5', [["Product 13", 13000], ["Product 14", 14000], ["Product 15", 15000]]),
+];
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  tableWrapper: {
+    maxHeight: 440,
+    overflow: 'auto',
+  },
+});
+
+
+export default function StickyHeadTable() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <div className={classes.tableWrapper}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+                <TableCell
+                  key={columns[0].id}
+                  align={columns[0].align}
+                  style={{ minWidth: columns[0].minWidth }}
+                >
+                  {columns[0].label}
+                </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+              return (
+                <div className={classes.root}>
+                  <ExpansionPanel>
+                    <ExpansionPanelSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      <Typography className={classes.heading}>{row.salesOrder}</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                      <Table className={classes.table} aria-label="simple table">
+                        <TableBody>
+                          {row.products.map(product => (
+                            <TableRow key={product[0]}>
+                              <TableCell component="th" scope="row">
+                                {product[0]}
+                              </TableCell>
+                              <TableCell align="right">{product[1]}</TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </ExpansionPanelDetails>
+                  </ExpansionPanel>
+                </div>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
