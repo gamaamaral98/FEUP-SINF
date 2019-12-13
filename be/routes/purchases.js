498b4d8d41d8b0922f223cca7d22a7e446892f46
joaovgamaamaral <joaovgamaamaral@gmail.com>
2019-12-12 16:02:33 -0800

    Registration of purchase orders to the entry warehouse functioning. With hardcoded value for now

--- be/routes/purchases.js
+++ be/routes/purchases.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var { request } = require('../utils/utils');
 
 const url = process.env.URL;
+const company = 'DUDA';
 
 var purchases = () => {
     return request('get', url + '/purchases/orders?', null);
@@ -11,6 +12,27 @@ var purchases = () => {
 router.get('/', function(req, res, next) {
     
     purchases()
+    .then((r) => {
+        res.json(r.data);
+    })
+    .catch((e) => {
+        res.json(e);
+    });
+});
+
+/*
+    Os parâmetros que têm de ir neste body têm este nome em cada purchase order: naturalKey, orderNature, quantity
+*/
+var placeOnWarehouse = () => {
+    //HARDCODED PARA TESTAR SÓ
+    var data = [ { SourceDocKey: "ECF.2019.4", SourceDocLineNumber: 1, quantity: 200 } ];
+    
+    return request('post', url + '/goodsreceipt/processOrders/' + company, data);
+}
+
+router.post('/entry', function(req, res, next) {
+    
+    placeOnWarehouse()
     .then((r) => {
         res.json(r.data);
     })
